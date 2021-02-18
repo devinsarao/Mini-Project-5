38f4d4622d29232cf6f97be2a56b7fad31fadb76
Devin Sarao <devin.sarao@patelhs.org>
2021-02-18 15:42:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    //scanner for greeting and intro
+  Scanner scan = new Scanner(System.in);
+  System.out.println("")
+
+
+    
+  }
+}
